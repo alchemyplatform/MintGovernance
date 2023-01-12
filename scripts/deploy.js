9d34382aeb08632eef47c0adde44e98a2a4f1f4c
Dan Nolan <dan.nolan@alchemy.com>
2023-01-12 01:01:13 -0500

    init

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const transactionCount = await owner.getTransactionCount();
+
+  // gets the address of the token before it is deployed
+  const futureAddress = ethers.utils.getContractAddress({
+    from: owner.address,
+    nonce: transactionCount + 1
+  });
+
+  const MyGovernor = await ethers.getContractFactory("MyGovernor");
+  const governor = await MyGovernor.deploy(futureAddress);
+
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const token = await MyToken.deploy(governor.address);
+
+  console.log(
+    `Governor deployed to ${governor.address}`,
+    `Token deployed to ${token.address}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
